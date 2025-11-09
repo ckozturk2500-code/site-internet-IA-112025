925fb0c03fe8679a04831e59e20dbf3aa6ff76a9
Cenk <ckozturk2500gmail.com>
2025-11-09 10:00:04 +0100

    Ajout wsgi.py pour finaliser déploiement

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI entry point for Render deployment
+This file is used by Gunicorn to start the Flask application
+"""
+import os
+import sys
+
+# Add the backend_api directory to the Python path
+sys.path.insert(0, os.path.dirname(__file__))
+
+# Import the Flask app from src.main
+from src.main import app
+
+if __name__ == '__main__':
+    app.run()
+
